93ffac4a8595b25899a8ba23ee5cd8f3982e86fe
Adrian <adrian.hall@yale.edu>
2024-01-31 21:53:35 -0500

    move udp-related code to udp.ino

--- sensata-ui-ethernet/udp.ino
+++ sensata-ui-ethernet/udp.ino
@@ -1,3 +1,62 @@
+#include <QNEthernet.h>
+using namespace qindesign::network;
+
+// Set your static IP configuration
+IPAddress
+    teensyIP(169, 254, 44, 72);  // First three numbers should match laptop IP
+IPAddress
+    gateway(169, 254, 44, 1);      // First three numbers should match laptop IP
+IPAddress subnet(255, 255, 0, 0);  // Should map laptop subnet
+
+// Set to the latest IP address that the Teensy receives a message from
+IPAddress remoteIP(0, 0, 0, 0);
+
+constexpr uint16_t kPort = 5190;  // Chat port
+
+EthernetUDP udp;
+
+void udpSetup() {
+    // Get Teensy mac address
+    uint8_t mac[6];
+    Ethernet.macAddress(mac);  // This is informative; it retrieves, not sets
+    printf(
+        "MAC = %02x:%02x:%02x:%02x:%02x:%02x\r\n", mac[0], mac[1], mac[2],
+        mac[3], mac[4], mac[5]
+    );
+
+    // Listen for link changes
+    Ethernet.onLinkState([](bool state) {
+        if (state) {
+            printf(
+                "[Ethernet] Link ON: %d Mbps, %s duplex, %s crossover\r\n",
+                Ethernet.linkSpeed(),
+                Ethernet.linkIsFullDuplex() ? "full" : "half",
+                Ethernet.linkIsCrossover() ? "is" : "not"
+            );
+        } else {
+            printf("[Ethernet] Link OFF\r\n");
+        }
+    });
+
+    // Begin Ethernet connection with static IP address
+    Ethernet.setDHCPEnabled(false);
+    Ethernet.begin(teensyIP, subnet, gateway);
+
+    // Print Teensy IP
+    IPAddress ip = Ethernet.localIP();
+    printf("    Local IP     = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+
+    if (!Ethernet.waitForLink(15000)) {
+        printf("Failed to get link \r\n");
+        return;
+    } else {
+        printf("link established \r\n");
+    }
+
+    // Start UDP server
+    udp.begin(kPort);
+}
+
 void udpSend(const std::string &message) {
     udp.beginPacket(remoteIP, kPort);
     udp.print(message.c_str());
